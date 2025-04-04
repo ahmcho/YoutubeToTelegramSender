3cf4ed2637884e081313d9bf23615bb4be299ecd
Ahmad Cholluyev <ahmad.cholluyev@aztelekom.az>
2025-04-04 18:30:23 +0400

    [FIX] cleanups

--- background.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onInstalled.addListener((e) => {
-    console.log('Welcome');
-});
-chrome.storage.sync.get('isVideoPlayerPage', ({isVideoPlayerPage}) => {
-    console.log('is video player page');
-});
